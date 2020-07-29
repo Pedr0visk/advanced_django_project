a61b5d91f8e9ed4a5c4500a80109bd1b7736fad9
Pedro Henrique <pedro357bm@gmail.com>
2020-07-29 19:37:42 -0300

    class Campaign created with migrations

--- apps/bops/models.py
+++ apps/bops/models.py
@@ -1,26 +1,4 @@
 from django.db import models
 
-class Campaign(models.Model):
 
-  class StatusCampaign(models.TextChoices):
-    GREEN = 'G', _('Green')
-    YELLOW = 'Y', _('Yellow')
-    ORANGE = 'O', _('Orange')
-    RED = 'R', _('Red')
-
-
-  bop_id = models.ForeignKey(Bop, on_delete=models.PROTECT)
-  name = models.CharField(max_length=100)
-  description = models.CharField(max_length=100, null=True)
-  rig_name = models.CharField(max_length=100, null=True)
-  active = models.BooleanField(default=True)
-  status = models.CharField(
-    max_length=1,
-    choices=StatusCampaign.choices,
-    default=StatusCampaign.GREEN
-  )
-  start_date = models.DateTimeField(auto_now_add=False, auto_now=False, blank=True)
-  end_date
-
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
+  
--- apps/campaigns/models.py
+++ apps/campaigns/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class Campaign(models.Model):
+
+  class StatusCampaign(models.TextChoices):
+    GREEN   = 1, _('Green')
+    YELLOW  = 2, _('Yellow')
+    ORANGE  = 3, _('Orange')
+    RED     = 4, _('Red')
+
+  name = models.CharField(max_length=100)
+  # bop_id = models.ForeignKey(Bop, on_delete=models.PROTECT)
+  active = models.BooleanField(default=True)
+  rig_name = models.CharField(max_length=100, null=True)
+  description = models.TextField(null=True)
+
+  status = models.CharField(
+    max_length=1,
+    choices=StatusCampaign.choices,
+    default=StatusCampaign.GREEN
+  )
+
+  start_date = models.DateTimeField()
+  end_date = models.DateTimeField()
+
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return self.name
