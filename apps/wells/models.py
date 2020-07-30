5a127a3dfb45e236306129bcf52aab09a1b0f04b
Pedro Henrique <pedro357bm@gmail.com>
2020-07-30 13:50:52 -0300

    model well created

--- apps/wells/models.py
+++ apps/wells/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from apps.campaigns.models import Campaign
 
-# Create your models here.
+class Well(models.Model):
+  campaign = models.OneToOneField(Campaign, on_delete=models.CASCADE)
+  name = models.CharField(max_length=255)
+  lon = models.FloatField()
+  lat = models.FloatField()
+
+  def __str__(self):
+    return self.name
