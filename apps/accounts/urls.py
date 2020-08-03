f06a377a5edc8d95bdb89ca023971f23840c6f05
Pedro Henrique <pedro357bm@gmail.com>
2020-08-03 14:59:53 -0300

    created decorations for user unathenticated and allowed users

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  path('', views.dashboard, name="dashboard"),
   path('login/', views.loginPage, name="login"),
   path('register/', views.registerPage, name="register"),
   path('logout/', views.logoutUser, name="logout"),
-  path('', views.dashboard, name="dashboard"),
+  path('users-list/', views.usersList, name="list_users"),
 ]
--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -1,23 +1,31 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User 
 
 from django.contrib.auth import authenticate, login, logout 
 from django.contrib import messages
 
+from .forms import CreateUserForm
+from .decorators import unauthenticated_user, allowed_users
+
 def registerPage(request):
-  form = UserCreationForm()
+  form = CreateUserForm()
 
   if request.method == 'POST':
-    form = UserCreationForm(request.POST)
+    form = CreateUserForm(request.POST)
     if form.is_valid():
       form.save()
+      user = form.cleaned_data.get('username')
+      messages.success(request, 'Account created Successfully for: ' + user)
+      
+      return redirect('register')
 
   context = {'form': form}
   return render(request, 'accounts/register.html', context)
 
 
+@unauthenticated_user
 def loginPage(request):
 
   if request.method == 'POST':
@@ -43,3 +51,10 @@ def logoutUser(request):
 
 def dashboard(request):
   return render(request, 'accounts/dashboard.html')
+
+
+@allowed_users(allowed_roles=['Admin'])
+def usersList(request):
+  users = User.objects.all()
+  context = {'users': users}
+  return render(request, 'accounts/users-list.html', context)
