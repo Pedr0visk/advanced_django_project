fd7f367792e0362671067f5811d18fe053bff12d
Pedro Henrique <pedro357bm@gmail.com>
2020-08-04 11:43:53 -0300

    user can add group to employer

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -2,27 +2,34 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.forms import inlineformset_factory
 from django.contrib.auth import authenticate, login, logout 
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User, Group
 from django.contrib.auth.decorators import login_required
 
 from django.contrib import messages
 
 from .forms import CreateUserForm
+from .models import Employer
 from .decorators import unauthenticated_user, allowed_users
 
 def registerPage(request):
+
   form = CreateUserForm()
+  groups = Group.objects.all()
 
   if request.method == 'POST':
     form = CreateUserForm(request.POST)
     if form.is_valid():
-      form.save()
-      user = form.cleaned_data.get('username')
-      messages.success(request, 'Account created Successfully for: ' + user)
-      
+      group = Group.objects.get(pk=request.POST['group'])
+
+      user = form.save()
+      user.groups.add(group)
+      Employer.objects.create(user=user,)
+
+      username = form.cleaned_data.get('username')
+      messages.success(request, 'Account created Successfully for: ' + username)
       return redirect('register')
 
-  context = {'form': form}
+  context = {'form': form, 'groups': groups}
   return render(request, 'accounts/register.html', context)
 
 
