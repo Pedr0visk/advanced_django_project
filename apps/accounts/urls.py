bc76ecc6d63bc4746044dae0af05309661b487fe
Pedro Henrique <pedro357bm@gmail.com>
2020-08-12 01:41:15 -0300

    standart methods for reset password added

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,10 +1,16 @@
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-  path('', views.dashboard, name="dashboard"),
-  path('login/', views.login_page, name="login"),
-  path('logout/', views.logout_user, name="logout"),
-  path('profile/change/', views.profile_update, name="update_profile"),
-  path('profile/password_change/', views.password_change, name="password_change")
+    path('', views.dashboard, name="dashboard"),
+    path('login/', views.login_page, name="login"),
+    path('logout/', views.logout_user, name="logout"),
+    path('profile/change/', views.profile_update, name="update_profile"),
+    path('profile/password_change/', views.password_change, name="password_change"),
+
+    path('profile/reset_password/', auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path('profile/reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('profile/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('profile/reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete")
 ]
