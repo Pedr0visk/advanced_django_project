593fc172e46907faa304dcf97e6b7e737591bf1c
Pedro Henrique <pedro357bm@gmail.com>
2020-08-26 15:20:51 -0300

    listing bops

--- apps/bops/views.py
+++ apps/bops/views.py
@@ -4,12 +4,19 @@ from django.contrib.auth.decorators import login_required
 
 from .models import Bop, SafetyFunction, Component, FailureMode, Cut
 from .forms import BopForm
+from apps.managers.decorators import allowed_users
 
-from apps.campaigns.forms import CampaignForm
 
-
+@allowed_users(allowed_roles=['Admin'])
 def upload(request):
     form = BopForm()
     context = {'form': form}
 
     return render(request, 'bops/bop_form.html', context)
+
+
+@allowed_users(allowed_roles=['Admin'])
+def bop_list(request):
+    bop_queryset = Bop.objects.all()
+    context = {'bops': bop_queryset}
+    return render(request, 'bops/bop_list.html', context)
