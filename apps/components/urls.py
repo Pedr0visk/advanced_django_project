e607d896b3e9850a28f70288be05cc6def656e33
Pedro Henrique <pedro357bm@gmail.com>
2020-09-01 12:43:22 -0300

    list failure modes for component and index component page

--- apps/components/urls.py
+++ apps/components/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.component_list, name='list_components'),
+    path('<slug:slug>/', views.index, name='index_component'),
 ]
--- apps/components/views.py
+++ apps/components/views.py
@@ -11,3 +11,11 @@ def component_list(request, bop_pk):
 
     context = {'bop': bop, 'components': components}
     return render(request, 'components/component_list.html', context)
+
+
+def index(request, bop_pk, slug):
+    bop = Bop.objects.get(pk=bop_pk)
+    component = Component.objects.get(slug=slug)
+
+    context = {'bop': bop, 'component': component}
+    return render(request, 'components/index.html', context)
