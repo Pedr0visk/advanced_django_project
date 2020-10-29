722836d03b5029e1b212fe8555e18b21283f0126
Pedro Henrique <pedro357bm@gmail.com>
2020-10-29 10:01:32 -0300

    fixed dockerfile for development

--- app/bop/settings.py
+++ app/bop/settings.py
@@ -17,6 +17,8 @@ from pathlib import Path  # Python 3.6+ only
 env_path = Path('.') / '.env.dev'
 load_dotenv(dotenv_path=env_path)
 
+print(os.environ.get("SECRET_KEY"))
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
