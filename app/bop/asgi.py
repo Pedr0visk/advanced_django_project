a72b139676edf92d11e426a73f02026d12e35dfb
Dan <and.dan19@gmail.com>
2021-01-19 14:02:00 -0300

    Minor adjusts

--- app/bop/asgi.py
+++ app/bop/asgi.py
@@ -6,31 +6,27 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 """
+
 import os
 
-from django.conf.urls import url
-from django.core.asgi import get_asgi_application
-# Fetch Django ASGI application early to ensure AppRegistry is populated
-# before importing consumers and AuthMiddlewareStack that may import ORM
-# models.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bop.settings')
-django_asgi_app = get_asgi_application()
-print(django_asgi_app)
-
-from apps.notifications.consumer import NotificationConsumer
-from apps.campaigns.consumer import CampaignConsumer
-from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.core.asgi import get_asgi_application
+from django.urls import path
 
+from apps.campaigns.consumer import CampaignConsumer
+from apps.notifications.consumer import NotificationConsumer
 
-
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bop.settings')
 
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter([
-            url(r"^ws/notifications/$", NotificationConsumer.as_asgi()),
-            url(r"^ws/campaigns/$", CampaignConsumer.as_asgi()),
+            path('ws/notifications/<int:user_id>/',
+                 NotificationConsumer.as_asgi()),
+            path('ws/campaigns/<int:user_id>/',
+                 CampaignConsumer.as_asgi()),
         ]),
     ),
 })
